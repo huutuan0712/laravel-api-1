260dba633be99dbfef8ee04e210b60c8a342d026
huutuan0712 <tuan071220@gmail.com>
2022-06-30 17:01:44 +0700

    commit 4

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->tinyInteger('role')->default('0');
+            $table->string('avatar');
             $table->rememberToken();
             $table->timestamps();
         });
